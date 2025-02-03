34291a802f879093ae4b2b48fffd18434c583e4b
Ricardo Nogueira <ricardonogueirajesus@gmail.com>
2025-02-02 21:45:34 -0500

    Additional files added into partials template

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,7 @@ def home(request):
     #Rendering web applications. 
     #recipes/ points to the path of the templates, such as /recipes or global/.
     #This allows you to reuse files with the same name by simply pointing to the template path.
-    return render(request,'recipes/home.html', context={
+    return render(request,'recipes/pages/home.html', context={
         'name': 'Ricardo Nogueira'
     })
 
